4ef83e5f453772eff9abf9e85d9537e607ac69f9
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-11-26 22:45:58 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,22 @@
+class PigLatinizer
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def piglatinize
+    letters = word.split("")
+    first = ""
+    middle = ""
+
+    letters.each_with_index do |letter, index|
+      first = word.slice(index, word.size)
+      middle = word.slice(0, index)
+      break if letter.scan(/[aeiou]/) != []
+    end
+    
+    first + middle + "ay"
+  end
+
+end
