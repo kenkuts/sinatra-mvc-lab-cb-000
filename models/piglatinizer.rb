4f5b7141d9507a3e099a6cf567da8f3dd995dbee
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-11-26 23:27:24 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,16 +13,18 @@ class PigLatinizer
     letters = word.split("")
     if letters.size == 1
       individual_word(letters)
-    first = ""
-    middle = ""
+    else
+      first = ""
+      middle = ""
 
-    letters.each_with_index do |letter, index|
-      first = word.slice(index, word.size)
-      middle = word.slice(0, index)
-      break if letter.scan(/[aeiou]/) != []
+      letters.each_with_index do |letter, index|
+        first = word.slice(index, word.size)
+        middle = word.slice(0, index)
+        break if letter.scan(/[aeiou]/) != []
+      end
+
+      first + middle + "ay"
     end
-
-    first + middle + "ay"
   end
 
   def individual_word(word)
