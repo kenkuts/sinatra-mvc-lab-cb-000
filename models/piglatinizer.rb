a9ee400280f6a7bb1f1272ec1be3defcd065bcee
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-11-27 00:09:55 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -14,7 +14,7 @@ class PigLatinizer
   def piglatinize_word(word)
     letters = word.first.split("")
 
-    if letters.size == 1            # If theres only one letter in the word
+    if letters.first.scan(/[aeiouAEIOU]/) != nil            # If theres only one letter in the word
       individual_word(letters)      # it adds 'way' at the end of the string
 
     else
